7d488b90bd9ce674b06ae98840eac5467c864fdf
Tamas Fabian <tamas.fabian@dealogic.com>
2022-02-02 21:33:15 +0100

    Works for konyvmolykepzo

--- app.py
+++ app.py
@@ -1,23 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ekonyv.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ebooks.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Ebook(db.Model):
+
+    #booktitle + "," + booklink + "," + bookprice + "," + imglink +"\n"
+
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    author = db.Column(db.String(300), nullable=False)
+    title = db.Column(db.String(300), nullable=False)
+    link = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Integer, nullable=True)
+    coverlink = db.Column(db.String(200), nullable=True)
+    source = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    books = Ebook.query.order_by(Ebook.author).all()
+    return render_template('index.html', books=books)
 
+
+@app.route('/process', methods=['POST', 'GET'])
+def process():
+    
+    with open("ebooks.txt", encoding="utf-8") as f:
+        lines = [line.rstrip('\n') for line in f]
+    
+
+    for line in lines:
+
+        data = line.split("|")
+
+        new_book = Ebook(author=data[0],title=data[1],link=data[2],price=data[3],coverlink=data[4],source=data[5])
+
+        try:
+            db.session.add(new_book)
+            db.session.commit()
+
+        except:
+            pass
+
+    return 'DONE'
 
 if __name__ == "__main__":
     app.run(debug=True)
